5db3914c13e8d6d6ca182252aa691d16abb120b7
Atanas-Valkov <nas-1986@hotmail.com>
2025-03-08 15:22:59 +0200

    Update Program.cs

--- 2.CSharp-Fundamentals/3.3Arrays-MORE-EXERCISE/P03ME1.EncryptSortAndPrintArray/Program.cs
+++ 2.CSharp-Fundamentals/3.3Arrays-MORE-EXERCISE/P03ME1.EncryptSortAndPrintArray/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace P03ME1.EncryptSortAndPrintArray
+﻿namespace P03ME1.EncryptSortAndPrintArray;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int numberOfStrings = int.Parse(Console.ReadLine());
+        string[] names = new string[numberOfStrings];
+        int[] ascendingOrder = new int[numberOfStrings];
+        int sum = 0;
+
+        for (int i = 0; i < numberOfStrings; i++)
         {
-            
+            names[i] = Console.ReadLine();
+
+            sum = 0;
+            foreach (var c in names[i])
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                {
+                    sum += (int)c * names[i].Length;
+                    ascendingOrder[i] = sum;
+                }
+                else
+                {
+                    sum += (int)c / names[i].Length;
+                    ascendingOrder[i] = sum;
+                }
+            }
+        }
+        Array.Sort(ascendingOrder);
+        foreach (var num  in ascendingOrder)
+        {
+            Console.WriteLine(num);
         }
     }
 }

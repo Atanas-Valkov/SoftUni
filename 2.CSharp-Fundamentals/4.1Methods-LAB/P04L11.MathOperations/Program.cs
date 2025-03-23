7461f5a86668d4e26443c260360350b74a969fc7
Atanas-Valkov <nas-1986@hotmail.com>
2025-03-23 11:39:46 +0200

    Update Program.cs

--- 2.CSharp-Fundamentals/4.1Methods-LAB/P04L11.MathOperations/Program.cs
+++ 2.CSharp-Fundamentals/4.1Methods-LAB/P04L11.MathOperations/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
 namespace P04L11.MathOperations
 {
@@ -6,9 +7,35 @@ namespace P04L11.MathOperations
     {
         static void Main(string[] args)
         {
+            double firstNumber = double.Parse(Console.ReadLine());
+            string commandLine = Console.ReadLine();
+            double secondNumber = double.Parse(Console.ReadLine());
 
-            lplpll;p
+            MathOperationt(firstNumber, secondNumber, commandLine);
+            Console.WriteLine(MathOperationt(firstNumber, secondNumber, commandLine));
+        }
 
+        private static double MathOperationt(double firstNumber, double secondNumber, string? commandLine)
+        {
+            double sum = 0;
+            if (commandLine == "+")
+            {
+                sum = firstNumber + secondNumber;
+            }
+            else if (commandLine == "-")
+            {
+                sum = firstNumber - secondNumber;
+            }
+            else if (commandLine == "*")
+            {
+                sum = firstNumber * secondNumber;
+            }
+            else if (commandLine == "/")
+            {
+                sum = firstNumber - secondNumber;
+            }
+
+            return sum;
         }
     }
 }

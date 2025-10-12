a965eadf7280da34e1e5d5d607f65144b0a40cb5
Atanas-Valkov <nas-1986@hotmail.com>
2025-10-12 19:59:37 +0300

    Added a solution

---
+++ 4. C# OOP/1.2 Inheritance EXERCISE/Person/Person .cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Person;
+
+public class Person
+{
+    private int age;
+    private string name;
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+    public virtual int Age
+    {
+        get => this.age;
+        set
+        {
+            if (value > 0)
+            {
+                this.age = value;
+            }
+        }
+    }
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.GetType().Name} -> Name: {this.Name}, Age: {this.Age}";
+    }
+}
---
+++ 4. C# OOP/1.2 Inheritance EXERCISE/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Elf elf = new Elf("Elf", 10);
+            MuseElf museElf = new MuseElf("MuseElf", 15);
+            Console.WriteLine(elf.ToString());
+        }
+    }
+}

6f82e527b9bf9a3d029918c1efeb23fa0b97ac7a
Atanas-Valkov <nas-1986@hotmail.com>
2025-10-18 19:09:01 +0300

    Added a solution

---
+++ 4. C# OOP/2.1 Encapsulation LAB/P02L03.Validation/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                decimal salary = decimal.Parse(input[3]);
+
+                Person person = new Person(firstName, lastName, age, salary);
+                persons.Add(person);
+            }
+
+            int percentage = int.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(percentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+        }
+    }
+}
---
+++ 4. C# OOP/2.1 Encapsulation LAB/P2L04.Team/Team.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+using System.Drawing;
+
+namespace PersonsInfo;
+
+public class Team
+{
+    private string name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(string name)
+    {
+        this.Name = name;
+        this.firstTeam = new List<Person>();
+        this.reserveTeam = new List<Person>();
+    }
+
+    public string Name
+    {
+        get => this.name;
+        set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Name cannot contain fewer than 3 symbols!");
+            }
+
+            this.name = value;
+        }
+    }
+
+    public IReadOnlyCollection<Person> FirstTeam
+    {
+        get => this.firstTeam.AsReadOnly();
+    }
+
+    public IReadOnlyCollection<Person> ReserveTeam
+    {
+        get => this.reserveTeam.AsReadOnly();
+    }
+
+    public void AddPlayer(Person person)
+    {
+        if (person.Age < 40)
+        {
+            this.firstTeam.Add(person);
+        }
+        else
+        {
+            this.reserveTeam.Add(person);
+        }
+    }
+}

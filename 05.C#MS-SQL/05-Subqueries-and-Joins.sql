243db31018eb232f40c1120feff53d4e578ba893
Atanas-Valkov <nas-1986@hotmail.com>
2026-02-15 10:13:52 +0200

    Added a solution

--- 05.C#MS-SQL/05-Subqueries-and-Joins.sql
+++ 05.C#MS-SQL/05-Subqueries-and-Joins.sql
@@ -40,7 +40,171 @@ GO
 
 --3 Sales Employee
 
-SELECT *
-  FROM [Employees]
+   SELECT [e].[EmployeeID],
+          [e].[FirstName],
+          [e].[LastName],
+          [d].[Name]
+       AS [DepartmentName]
+     FROM [Employees]
+       AS [e]
+LEFT JOIN [Departments]
+       AS [d]
+       ON [e].[DepartmentID] = [d].[DepartmentID]
+    WHERE [d].[Name] = 'Sales'   
+ ORDER BY [EmployeeID] ASC 
 
+ GO 
+ 
+ --4 Employee Departments
+
+  SELECT TOP (5)
+         [e].[EmployeeID],
+         [e].[FirstName],
+         [e].[Salary],
+         [d].[Name]
+      AS [DepartmentName]
+    FROM [Employees]
+      AS [e]
+    JOIN [Departments]
+      AS [d]
+      ON [e].[DepartmentID] = [d].[DepartmentID]
+   WHERE [e].[Salary] > 15000
+ORDER BY [d].[DepartmentID] ASC
+
+GO
+
+--5 Employees Without Project
+
+SELECT TOP (3)
+           [e].[EmployeeID],
+           [e].[FirstName]
+      FROM [Employees]
+        AS [e]
+ LEFT JOIN [EmployeesProjects]
+        AS [ep]
+        ON [ep].[EmployeeID] = [e].[EmployeeID]
+     WHERE [ep].[ProjectID] IS NULL 
+
+GO
+
+--6 Employees Hired After
+
+  SELECT [e].[FirstName],
+         [e].[LastName],
+         [e].[HireDate],
+         [d].[Name]
+      AS [DepName]
+    FROM [Employees]
+      AS [e]
+    JOIN [Departments]
+      AS [d]
+      ON [e].[DepartmentID] = [d].DepartmentID
+   WHERE [HireDate] > '1.1.1999'
+     AND [d].[Name] IN ('Sales','Finance')
+ORDER BY [HireDate] ASC
+
+GO
+
+--7 Employees with Project
+
+SELECT TOP (5)
+           [e].[EmployeeID],
+           [e].[FirstName],
+           [p].[Name]
+        AS [ProjectName]
+      FROM [EmployeesProjects]
+        AS [ep]
+      JOIN [Employees]
+        AS [e]
+        ON [e].[EmployeeID] = [ep].[EmployeeID]
+      JOIN [Projects]
+        AS [p]
+        ON [ep].ProjectID = [p].[ProjectID]
+     WHERE [p].[StartDate] > '2002-08-13'
+       AND [p].[EndDate] IS NULL 
+  ORDER BY [e].[EmployeeID] ASC
+
+  GO 
+
+--8 Employee 24
+
+   SELECT [e].[EmployeeID],
+          [e].[FirstName],
+          CASE
+              WHEN [p].[StartDate] >= '2005'THEN NULL
+              ELSE [p].[Name]
+          END
+       AS [ProjectName]
+     FROM [Projects]
+       AS [p]
+LEFT JOIN [EmployeesProjects]
+       AS [ep]
+       ON [p].[ProjectID] = [ep].ProjectID
+LEFT JOIN [Employees]
+       AS [e]
+       ON [ep].EmployeeID = [e].EmployeeID
+    WHERE [e].[EmployeeID] = 24
+
+GO 
+
+--9 Employee Manager
+
+   SELECT [e].[EmployeeID],
+          [e].[FirstName],
+          [e].[ManagerID],
+          [m].[FirstName]
+       AS [ManagerName]
+     FROM [Employees]
+       AS [e]
+LEFT JOIN [Employees]
+       AS [m]
+       ON [e].[ManagerID] = [m].[EmployeeID]
+    WHERE [e].ManagerID IN (3,7)
+ ORDER BY [e].[EmployeeID]
+
+GO 
+
+--10 Employees Summary
+
+SELECT TOP (50)
+           [e].[EmployeeID],
+           CONCAT_WS(' ', [e].[FirstName],[e].[LastName])
+        AS [EmployeeName],
+           CONCAT_WS(' ',[m].[FirstName], [m].[LastName])
+        AS [ManagerName],
+           [d].[Name]
+        AS [DepartmentName]
+      FROM [Employees]
+        AS [e]
+ LEFT JOIN [Employees]
+        AS [m]
+        ON [e].[ManagerID] = [m].[EmployeeID]
+ LEFT JOIN [Departments]
+        AS [d]
+        ON [e].[DepartmentID] = [d].[DepartmentID]
+  ORDER BY [e].EmployeeID ASC
+
+GO
+
+--11 Min Average Salary
+
+SELECT TOP (1)
+         AVG([Salary])
+      AS [MinAverageSalary]
+    FROM [Employees]
+GROUP BY [DepartmentID]
+ORDER BY [MinAverageSalary]
+
+
+
+
+
+  SELECT * 
+    FROM [Projects]
+
+  SELECT * 
+    FROM [Employees]
+
+  SELECT * 
+    FROM [Departments]
 

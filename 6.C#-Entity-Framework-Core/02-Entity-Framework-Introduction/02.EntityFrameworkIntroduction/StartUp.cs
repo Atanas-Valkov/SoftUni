c10ef0938090e57e90675b5e6a65345405e3d828
Atanas-Valkov <nas-1986@hotmail.com>
2026-03-11 23:08:20 +0200

    Added a solution

--- 6.C#-Entity-Framework-Core/02-Entity-Framework-Introduction/02.EntityFrameworkIntroduction/StartUp.cs
+++ 6.C#-Entity-Framework-Core/02-Entity-Framework-Introduction/02.EntityFrameworkIntroduction/StartUp.cs
@@ -1,6 +1,9 @@
-using System.Text;
+using System.Data.Common;
+using System.Globalization;
 using SoftUni.Data;
 using SoftUni.Models;
+using System.Reflection;
+using System.Text;
 
 namespace SoftUni
 {
@@ -14,10 +17,14 @@ namespace SoftUni
             //03 string result = GetEmployeesFullInformation(context); 
 
             //04 string result = GetEmployeesWithSalaryOver50000(context);
-            
+
             //05 string result = GetEmployeesFromResearchAndDevelopment(context);
 
-            string result = AddNewAddressToEmployee(context);
+            //06 string result = AddNewAddressToEmployee(context);
+
+            //07 string result = GetEmployeesInPeriod(context);
+
+             string result = GetAddressesByTown(context);
 
             Console.WriteLine(result);
         }
@@ -48,7 +55,7 @@ namespace SoftUni
         }
 
 
-        // 4.	Employees with Salary Over 50 000 
+        // 4. Employees with Salary Over 50 000 
         public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
         {
             StringBuilder sb = new StringBuilder();
@@ -71,7 +78,7 @@ namespace SoftUni
             return sb.ToString().TrimEnd();
         }
 
-        // 05.	Employees from Research and Development
+        // 05. Employees from Research and Development
 
         public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
         {
@@ -108,8 +115,8 @@ namespace SoftUni
 
             var employee = context.Employees
                 .First(e => e.LastName == "Nakov");
-                
-                
+
+
             Address newAddress = new Address()
             {
                 AddressText = "Vitoshka 15",
@@ -128,5 +135,80 @@ namespace SoftUni
             return string.Join(Environment.NewLine, employeeFiltering);
         }
 
+        //07. Employees and Projects
+
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employee = context.Employees
+                .Take(10)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    ManagerFirstName = e.Manager!.FirstName,
+                    ManagerLastName = e.Manager.LastName,
+                    Projects = e.EmployeesProjects
+                        .Where(ep => ep.Project.StartDate.Year >= 2001 &&
+                                                               ep.Project.StartDate.Year <= 2003)
+                    .Select(ep => new
+                    {
+                        ep.Project.Name,
+                        ep.Project.StartDate,
+                        ep.Project.EndDate
+                    })
+                    .ToList()
+                })
+                .ToList();
+
+            foreach (var e in employee)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} - Manager: {e.ManagerFirstName} {e.ManagerLastName}");
+               foreach (var p in e.Projects)
+               {
+                   string endDate = "";
+                   if (p.EndDate.HasValue)
+                   {
+                       endDate = p.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt");
+                   }
+                   else
+                   {
+                       endDate = "not finished";
+                   }
+
+                   sb.AppendLine($"--{p.Name} - {p.StartDate.ToString("M/d/yyyy h:mm:ss tt")} - {endDate}");
+               }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //08. Addresses by Town
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var addresses = context.Addresses
+                .OrderByDescending(a => a.Employees.Count)
+                .ThenBy(a => a.Town!.Name)
+                .ThenBy(a => a.AddressText)
+                .Take(10)
+                .Select(a => new
+                {
+                    a.AddressText,
+                    TownName = a.Town.Name,
+                    EmployeesCount = a.Employees.Count
+                })
+                .ToList();
+
+            foreach (var a in addresses)
+            {
+                sb.AppendLine($"{a.AddressText}, {a.TownName} - {a.EmployeesCount} employees");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }

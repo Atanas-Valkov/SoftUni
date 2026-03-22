acf050299cd333651d07e3dbefa59f485f7f3b5f
Atanas-Valkov <nas-1986@hotmail.com>
2026-03-22 14:19:10 +0200

    Added a solution

---
+++ 6.C#-Entity-Framework-Core/04-LINQ/04.LINQ-Exercises-MusicHub-6.0/MusicHub/Data/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicHub.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=localhost,1433;Database=MusicHub;User Id = sa; Password=SoftUn!2026;TrustServerCertificate=True;Encrypt=False;";
+    }
+    
+}

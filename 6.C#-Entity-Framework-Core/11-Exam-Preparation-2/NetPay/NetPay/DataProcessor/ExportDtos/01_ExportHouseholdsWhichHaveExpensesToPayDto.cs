5c4724f1d911eac405810e45f5e6dc4f480b0a8a
AtanasValkov <AtanasValkov@students.softuni.bg>
2026-05-02 14:12:00 +0300

    Added a solution

---
+++ 6.C#-Entity-Framework-Core/11-Exam-Preparation-2/NetPay/NetPay/DataProcessor/ExportDtos/01_ExportHouseholdsWhichHaveExpensesToPayDto.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace NetPay.DataProcessor.ExportDtos;
+
+[XmlType("Household")]
+public class ExportHouseholdsWhichHaveExpensesToPayDto
+{
+    //<Households>                                         ==>XmlRoot 
+    // <Household>                                         ==>XmlType 
+    //   <ContactPerson>Alexander Ivanov</ContactPerson>   ==>XmlElement
+    //   <Email>alexander.ivanov@example.com</Email>       ==>XmlElement
+    //   <PhoneNumber>+144/123-123456</PhoneNumber>        ==>XmlElement
+    //   <Expenses>                                        ==>XmlArr
+    //     <Expense>                                       ==>XmlType
+    //       <ExpenseName>Water Home</ExpenseName>         ==>XmlElement
+    //       <Amount>50.50</Amount>                        ==>XmlElement
+    //       <PaymentDate>2024-08-20</PaymentDate>         ==>XmlElement
+    //       <ServiceName>Water</ServiceName>              ==>XmlElement
+    //     </Expense>                                      ==>
+    //     <Expense>                                       ==>
+    //       <ExpenseName>Electricity Home</ExpenseName>   ==>
+    //       <Amount>120.50</Amount>                       ==>
+    //       <PaymentDate>2024-08-25</PaymentDate>         ==>
+    //       <ServiceName>Electricity</ServiceName>        ==>
+    //     </Expense>
+    
+    [XmlElement("ContactPerson")]
+    public string ContactPerson { get; set; } = null!;
+
+    
+    [XmlElement("Email")]
+    public string? Email { get; set; } 
+
+    
+    [XmlElement("PhoneNumber")]
+    public string PhoneNumber { get; set; } = null!;
+
+    [XmlArray("Expenses")] 
+    public ExpensesToPayDto[] Expenses { get; set; } = null!;
+}
--- 6.C#-Entity-Framework-Core/11-Exam-Preparation-2/NetPay/NetPay/DataProcessor/Serializer.cs
+++ 6.C#-Entity-Framework-Core/11-Exam-Preparation-2/NetPay/NetPay/DataProcessor/Serializer.cs
@@ -1,4 +1,11 @@
-﻿using NetPay.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using NetPay.Data;
+using NetPay.Data.Models.Enums;
+using NetPay.Data.Utilities;
+using NetPay.DataProcessor.ExportDtos;
+using Newtonsoft.Json;
+
+
 
 namespace NetPay.DataProcessor
 {
@@ -6,12 +13,69 @@ namespace NetPay.DataProcessor
     {
         public static string ExportHouseholdsWhichHaveExpensesToPay(NetPayContext context)
         {
-            throw new NotImplementedException();
+
+
+            ExportHouseholdsWhichHaveExpensesToPayDto[] query = context
+                .Households
+                .AsNoTracking()
+                .Include(h => h.Expenses)
+                .ThenInclude(h => h.Service)
+                .Where(h => h.Expenses.Any(e => e.PaymentStatus != PaymentStatus.Paid))
+                .OrderBy(h => h.ContactPerson)
+                .ToArray()
+                .Select(h => new ExportHouseholdsWhichHaveExpensesToPayDto()
+                {
+                    ContactPerson = h.ContactPerson,
+                    Email = h.Email,
+                    PhoneNumber = h.PhoneNumber,
+                    Expenses = h.Expenses
+                        .Where(e => e.PaymentStatus != PaymentStatus.Paid)
+                        .Select(e => new ExpensesToPayDto()
+                        {
+                            ExpenseName = e.ExpenseName,
+                            Amount = e.Amount.ToString("F2"),
+                            PaymentDate = e.DueDate.ToString("yyyy-MM-dd"),
+                            ServiceName = e.Service.ServiceName
+
+                        })
+                        .OrderBy(e => e.PaymentDate)
+                        .ThenBy(e => e.Amount)
+                        .ToArray()
+
+                })
+                .ToArray();
+
+            string xmlResult = XmlHelper
+                .Serialize(query, "Households");
+            return xmlResult;
         }
 
         public static string ExportAllServicesWithSuppliers(NetPayContext context)
         {
-            throw new NotImplementedException();
+            var exportAllServicesWithSuppliersQuery = context.Services
+                .AsNoTracking()
+                .Include(s => s.SuppliersServices)
+                .ThenInclude(s => s.Supplier)
+                .OrderBy(s => s.ServiceName)
+                .Select(s => new
+                {
+                    ServiceName = s.ServiceName,
+                    Suppliers = s.SuppliersServices
+                        .Select(ss => ss.Supplier)
+                        .OrderBy(sup => sup.SupplierName)
+                        .Select(sup => new
+                        {
+                            sup.SupplierName,
+
+                        })
+                        .ToArray()
+
+                })
+                .ToArray();
+
+            string jsonResult = JsonConvert.SerializeObject(exportAllServicesWithSuppliersQuery, Formatting.Indented);
+
+            return jsonResult;
         }
     }
 }
